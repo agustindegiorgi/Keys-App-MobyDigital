7b1e7611aa8bbea811462f2a32fe1a34385dee27
agustindegiorgi <agustindegiorgi@users.noreply.github.com>
2020-04-29 11:57:49 -0300

    ULT MAYUS

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,18 +10,18 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms'; //acá está 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { LoginComponent } from './login/login.component';
-import { LoginService } from "./service/login.service";
+import { LoginService } from "./Service/login.service";
 import { AdminComponent } from './admin/admin.component';
 import { UserComponent } from "./user/user.component";
-import { ListarUserComponent } from './user/lista/listar/listar-user.component';
+import { ListarUserComponent } from './user/Lista/listar/listar-user.component';
 import { HomeComponent } from "./home/home.component";
 import { RootComponent } from "./root/root.component";
-import { ListarComponent } from './admin/lista/listar/listar.component';
-import { AddComponent } from './admin/lista/add/add.component';
-import { EditComponent } from './admin/lista/edit/edit.component';
-import { PersonaService } from "./service/persona-service";
-import { VerDialogComponent } from "./admin/lista/listar/ver-dialog/ver-dialog.component";
-import { VerDialogUserComponent } from "./user/lista/listar/ver-dialog-user/ver-dialog-user.component";
+import { ListarComponent } from './admin/Lista/listar/listar.component';
+import { AddComponent } from './admin/Lista/add/add.component';
+import { EditComponent } from './admin/Lista/edit/edit.component';
+import { PersonaService } from "./Service/persona-service";
+import { VerDialogComponent } from "./admin/Lista/listar/ver-dialog/ver-dialog.component";
+import { VerDialogUserComponent } from "./user/Lista/listar/ver-dialog-user/ver-dialog-user.component";
 
 @NgModule({
   declarations: [
