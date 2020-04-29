223eeee18a88a6fed5de6d37a459bb8d65fe4309
agustindegiorgi <agustindegiorgi@users.noreply.github.com>
2020-04-29 12:18:52 -0300

    Request Heroku

--- client/src/app/Service/login.service.ts
+++ client/src/app/Service/login.service.ts
@@ -12,7 +12,7 @@ export class LoginService {
 
   login(username:string, password:string){
       const headers = new HttpHeaders({ Authorization: 'Basic ' + btoa(username + ':' + password) });
-      return this.http.get("http://localhost:8081?username="+username +"&password="+password,{headers,responseType: 'text' as 'json'})
+      return this.http.get("https://keys-app-mobydigital.herokuapp.com/?username="+username +"&password="+password,{headers,responseType: 'text' as 'json'})
     }
 
 }
