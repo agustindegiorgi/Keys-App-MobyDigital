3ba56c40817225ab3a8323358f87bf31be10f561
agustindegiorgi <agustindegiorgi@users.noreply.github.com>
2020-05-04 12:09:05 -0300

    Import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,21 @@
+INSERT INTO `users` (`dni`, `name`, `lastname`, `telephone`, `email`) VALUES ('34455701', 'Norberto', 'Magarzo', '351434343', 'nmagarzo@mobydigital.com');
+INSERT INTO `users` (`dni`, `name`, `lastname`, `telephone`, `email`) VALUES ('43369999', 'Agustín', 'De Giorgi', '3515183039', 'adegiorgi@mobydigital.com');
+INSERT INTO `users` (`dni`, `name`, `lastname`, `telephone`, `email`) VALUES ('24565323', 'Gabriel', 'Mamondes', '351783778', 'titán@mobydigital.com');
+INSERT INTO `users` (`dni`, `name`, `lastname`, `telephone`, `email`) VALUES ('25678786', 'Hector', 'Rattis', '351654323', 'hrattis@mobydigital.com');
+INSERT INTO `users` (`dni`, `name`, `lastname`, `telephone`, `email`) VALUES ('38994321', 'Pablo', 'Narvaja', '35198834', 'pnarvaja@mobydigital.com');
+INSERT INTO 'users' ('dni', 'name', 'lastname', 'telephone', 'email') VALUES ('25342678', 'Matías', 'Pérez', '351323454', 'mperez@mobydigital.com');
+INSERT INTO 'users' ('dni', 'name', 'lastname', 'telephone', 'email') VALUES ('29608509', 'Débora', 'Del Pórtico', '3535105423', 'ddelportico@mobydigital.com');
+INSERT INTO 'users' ('dni', 'name', 'lastname', 'telephone', 'email') VALUES ('33433212', 'Kiyo', 'Uehara', '351833293', 'kuehara@mobydigital.com');
+INSERT INTO `keysdoor` (`id`, `name`) VALUES (`1`, `cocina`);
+INSERT INTO `keysdoor` (`id`, `name`) VALUES (`2`, `entrada`);
+INSERT INTO `keysdoor` (`id`, `name`) VALUES (`3`, `porton`);
+INSERT INTO `doors` (`id`, `name`) VALUES (`1`, `patio`);
+INSERT INTO `doors` (`id`, `name`) VALUES (`2`, `cocina`);
+INSERT INTO `doors` (`id`, `name`) VALUES (`3`, `entrada`);
+INSERT INTO `doors` (`id`, `name`) VALUES (`4`, `porton`);
+INSERT INTO `doors` (`id`, `name`) VALUES (`5`, `patio`);
+INSERT INTO `login` (`dni`,`username`, `password`,`rol`) VALUES (`34455700`, `norberto`, `1234`, `ADMIN`);
+INSERT INTO `login` (`dni`,`username`, `password`,`rol`) VALUES (`34455701`, `agustin`, `1111`, `ADMIN`);
+INSERT INTO `login` (`dni`,`username`, `password`,`rol`) VALUES (`34455702`, `gabriel`, `2222`, `USER`);
+
+;
