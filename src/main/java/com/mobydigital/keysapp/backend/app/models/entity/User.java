8d8e4965d87a8ae1d9b4e282bc4bc349c10e4cd8
norbertomagarzo <norbertomagarzo@gmail.com>
2020-05-06 13:54:01 -0300

    relacion de tablas

--- src/main/java/com/mobydigital/keysapp/backend/app/models/entity/User.java
+++ src/main/java/com/mobydigital/keysapp/backend/app/models/entity/User.java
@@ -1,10 +1,14 @@
 package com.mobydigital.keysapp.backend.app.models.entity;
 
 import java.io.Serializable;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -19,6 +23,13 @@ public class User implements Serializable {
 	@Column(nullable = false)
 	private String lastname;
 	private String email;
+
+	@OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	private List<Key> keys;
+
+	public List<Key> getKey() {
+		return keys;
+	}
 
 	public Integer getDni() {
 		return dni;
