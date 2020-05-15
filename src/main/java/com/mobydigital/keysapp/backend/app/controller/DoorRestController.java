89bb85f3b066b48ecd23b151033d4adec11281c7
norbertomagarzo <norbertomagarzo@gmail.com>
2020-05-15 16:25:39 -0300

    encuentra llaves con columna dni null

--- src/main/java/com/mobydigital/keysapp/backend/app/controller/DoorRestController.java
+++ src/main/java/com/mobydigital/keysapp/backend/app/controller/DoorRestController.java
@@ -43,6 +43,7 @@ public class DoorRestController {
 		}
 
 	}
+	
 
 	@PostMapping("/door")
 	@ResponseStatus(HttpStatus.CREATED)
--- src/main/java/com/mobydigital/keysapp/backend/app/models/services/IDoorkeyService.java
+++ src/main/java/com/mobydigital/keysapp/backend/app/models/services/IDoorkeyService.java
@@ -5,12 +5,14 @@ import java.util.List;
 import com.mobydigital.keysapp.backend.app.models.entity.Doorkey;
 
 
+
 public interface IDoorkeyService  {
 	
 	public List<Doorkey> findAll();
 	public Doorkey findById(Integer id);
 	public Doorkey save(Doorkey doorkey);
 	public void deleteById(Integer id);
+	public List<Doorkey> findKeyDninull();
 	
 	
 
